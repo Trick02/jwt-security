fea96f13ba09fe752bdc645529c159cc8c744740
trick02 <tricknride@gmail.com>
2024-09-29 02:55:21 +0800

    first commit

---
+++ src/main/java/com/trick02/jwt_security/JwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.trick02.jwt_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trick02/jwt_security/JwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trick02.jwt_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
